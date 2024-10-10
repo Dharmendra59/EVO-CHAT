f42ab3c41b0ce27b97178b7098dc9c1770e3ea65
Dharmendra Singh <singhdhas9559604443a@gmail.com>
2024-10-10 07:48:35 +0530

    change

--- script.js
+++ script.js
@@ -5,14 +5,28 @@ let chatContainer = document.querySelector(".chat-container");
 let userMessage = null;
 let Api_Url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyB3H0HvQ1wPylcz4-MvyY-DbJAhagjEpgc'
 
+
 function createChatBox(html, className) {
     let div = document.createElement("div")
     div.classList.add(className)
     div.innerHTML = html
     return div
 }
+
+function speak(text) {
+    let text_speak = new SpeechSynthesisUtterance(text)
+    text_speak.rate = 1
+    text_speak.pitch = 1
+    text_speak.volume = 1
+    text_speak.lang = "hi-IN"
+    window.speechSynthesis.speak(text_speak)
+
+}
+
+
 async function getApiResponse(aiChatBox) {
     let textElement = aiChatBox.querySelector(".text")
+    let speaker = aiChatBox.querySelector("#peak")
     try {
         let response = await fetch(Api_Url, {
             method: "POST",
@@ -26,7 +40,39 @@ async function getApiResponse(aiChatBox) {
         })
         let data = await response.json();
         let apiResponse = data ? data.candidates[0].content.parts[0].text : "Sorry. I didn't get that. Please try again"
-        textElement.innerText = apiResponse
+        let aiResponse = apiResponse.split(" ")
+        let clutter = ""
+
+        aiResponse.forEach((text, index) => {
+            clutter += `<span class="aiReply">${text} </span>`;
+            textElement.innerHTML = clutter
+        })
+
+
+        textElement.innerHTML = clutter
+
+        let classHandler = () => {
+            let aiReply = document.querySelectorAll(".aiReply")
+            aiReply.forEach((reply) => {
+                reply.classList.remove("aiReply")
+                reply.classList.add("aiReplyOld")
+            })
+
+        }
+
+        gsap.from(".aiReply", {
+            opacity: 0,
+            duration: 1,
+            stagger: 0.1,
+            onComplete: () => {
+                classHandler();
+            }
+        })
+        speaker.addEventListener("click", () => {
+            // speak(text)
+            console.log(text.value)
+
+        })
 
 
     } catch (error) {
@@ -41,6 +87,7 @@ async function getApiResponse(aiChatBox) {
 function showLoading() {
     let html = `<div class="img">
                 <img src="ai.png" alt="ai" width="50px">
+                <img src="speaker.svg" alt="speaker" id="peak">
             </div>
 
 
